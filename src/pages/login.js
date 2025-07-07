62667b1dc87aa32d8cbdde52a5463c7cf282069e
khanndelwalharshit <hg9269748@gmail.com>
2025-07-07 18:29:18 +0530

    fonts and styles

--- src/pages/login.js
+++ src/pages/login.js
@@ -16,7 +16,7 @@ const Login = () => {
 
     try {
       const res = await axios.post("https://auth-backend-31a8.onrender.com/login", formData);
-      localStorage.setItem("token", res.data.token);  // ✅ store JWT
+      localStorage.setItem("token", res.data.token);  
       alert("Login successful!");
     } catch (err) {
       alert(err.response?.data?.message || "Login failed");
@@ -24,14 +24,14 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg w-80 shadow-md space-y-4">
+    <div className="flex justify-center items-center min-h-screen bg-sky-200">
+      <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg w-80 shadow-lg space-y-4">
         <h2 className="text-2xl font-bold text-center mb-4">Log In</h2>
 
         <input name="phone" onChange={handleChange} type="text" placeholder="Phone Number" required className="w-full p-2 border rounded" />
         <input name="password" onChange={handleChange} type="password" placeholder="Password" required className="w-full p-2 border rounded" />
 
-        <button type="submit" className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700">Log In</button>
+        <button type="submit" className="w-full bg-teal-400 text-white py-2 rounded hover:bg-teal-800">Log In</button>
       </form>
     </div>
   );
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -31,7 +31,7 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="flex justify-center items-center min-h-screen bg-violet-200">
       <form onSubmit={handleSubmit} className="bg-white p-6 rounded-lg w-96 shadow-md space-y-4">
         <h2 className="text-2xl font-bold text-center mb-4">Sign Up</h2>
         <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} className="w-full p-2 border rounded" required />
@@ -40,7 +40,7 @@ const SignUp = () => {
         <input type="email" name="email" placeholder="Email" onChange={handleChange} className="w-full p-2 border rounded" required />
         <input type="password" name="password" placeholder="Password" onChange={handleChange} className="w-full p-2 border rounded" required />
         <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} className="w-full p-2 border rounded" required />
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded w-full hover:bg-blue-600">Register</button>
+        <button type="submit" className="bg-fuchsia-400 text-white p-2 rounded w-full hover:bg-fuchsia-800">Register</button>
       </form>
     </div>
   );
